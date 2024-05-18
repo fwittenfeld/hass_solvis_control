f6bc6b9a88fd1e1169d674f62f804e493e15a30a
Felix Wittenfeld <fwittenf@techfak.uni-bielefeld.de>
2024-05-19 00:22:11 +0200

    Added more sensors, sorted and named by datasheet

--- custom_components/solvis_control/const.py
+++ custom_components/solvis_control/const.py
@@ -29,20 +29,221 @@ class ModbusFieldConfig:
 
 
 PORT = 502
+
+# Order and naming see: https://solvis-files.s3.eu-central-1.amazonaws.com/downloads-fk/regelung/sc-3/SC-3_ModBus_Schnittstellenbeschreibung.pdf
 REGISTERS = [
+    ModbusFieldConfig(  # ZirkulationBetriebsart
+        name="zirkulation_betriebsart",
+        address=2049,
+        unit="",
+        device_class=None,
+        state_class=None,
+        multiplier=1,
+        # data=("0", "1", "2", "3"),
+    ),
+    ModbusFieldConfig(  # VersionSC2
+        name="version_sc3",
+        address=32770,
+        unit="",
+        device_class=None,
+        state_class=None,
+        multiplier=1,
+        entity_category="diagnostic",
+    ),
+    ModbusFieldConfig(  # VersionNBG
+        name="version_nbg",
+        address=32771,
+        unit="",
+        device_class=None,
+        state_class=None,
+        multiplier=1,
+        entity_category="diagnostic",
+    ),
+    
+    ModbusFieldConfig( # tank_layer4_water_temp
+        name="temp_s1",
+        address=33024,
+        unit="°C",
+        device_class="temperature",
+        state_class="measurement",
+    ),
+    ModbusFieldConfig( # domestic_water_temp
+        name="temp_s2",
+        address=33025,
+        unit="°C",
+        device_class="temperature",
+        state_class="measurement",
+    ),
+    ModbusFieldConfig( # tank_layer1_water_temp
+        name="temp_s3",
+        address=33026,
+        unit="°C",
+        device_class="temperature",
+        state_class="measurement",
+    ),
+    ModbusFieldConfig( # tank_layer3_water_temp
+        name="temp_s4",
+        address=33027,
+        unit="°C",
+        device_class="temperature",
+        state_class="measurement",
+    ),
+    ModbusFieldConfig( # solar_heat_exchanger_out_temp
+        name="temp_s5",
+        address=33028,
+        unit="°C",
+        device_class="temperature",
+        state_class="measurement",
+        enabled_by_default=False,
+    ),
+    ModbusFieldConfig( # solar_heat_exchanger_in_temp
+        name="temp_s6",
+        address=33029,
+        unit="°C",
+        device_class="temperature",
+        state_class="measurement",
+        enabled_by_default=False,
+    ),
+    ModbusFieldConfig( # solar_water_temp
+        name="temp_s7",
+        address=33030,
+        unit="°C",
+        device_class="temperature",
+        state_class="measurement",
+        enabled_by_default=False,
+    ),
+    ModbusFieldConfig( # solar_collector_temp_1
+        name="temp_s8",
+        address=33031,
+        unit="°C",
+        device_class="temperature",
+        state_class="measurement",
+    ),
+    ModbusFieldConfig( # tank_layer2_water_temp
+        name="temp_s9",
+        address=33032,
+        unit="°C",
+        device_class="temperature",
+        state_class="measurement",
+        negative=True,
+        absolut_value=True,
+    ),
+    ModbusFieldConfig( # outdoor_air_temp
+        name="temp_s10",
+        address=33033,
+        unit="°C",
+        device_class="temperature",
+        state_class="measurement",
+    ),
+    ModbusFieldConfig( # circulation_return_temp
+        name="temp_s11",
+        address=33034,
+        unit="°C",
+        device_class="temperature",
+        state_class="measurement",
+        multiplier=0.1,
+        enabled_by_default=False,
+    ),
+    ModbusFieldConfig( 
+        name="temp_s12",
+        address=33035,
+        unit="°C",
+        device_class="temperature",
+        state_class="measurement",
+        enabled_by_default=False,
+    ),    
+    ModbusFieldConfig(
+        name="temp_s13",
+        address=33036,
+        unit="°C",
+        device_class="temperature",
+        state_class="measurement",
+        enabled_by_default=False,
+    ),
+    ModbusFieldConfig(
+        name="temp_s14",
+        address=33037,
+        unit="°C",
+        device_class="temperature",
+        state_class="measurement",
+        enabled_by_default=False,
+    ),
+    ModbusFieldConfig(# cold_water_temp, 10°C if no sensor is connected
+        name="temp_s15",
+        address=33038,
+        unit="°C",
+        device_class="temperature",
+        state_class="measurement",
+    ),
+    ModbusFieldConfig( # solar_collector_temp_2 (only for east-west system)
+        name="temp_s16",
+        address=33039,
+        unit="°C",
+        device_class="temperature",
+        state_class="measurement",
+        enabled_by_default=False,
+    ),
+
+    ModbusFieldConfig( # solar_water_flow
+        name="volumenstrom_s17",
+        address=33040,
+        unit="l/min",
+        device_class="speed",
+        state_class="measurement",
+        enabled_by_default=False,
+    ),
+    ModbusFieldConfig(  # domestic_water_flow
+        name="volumenstrom_s18",
+        address=33041,
+        unit="l/min",
+        device_class="speed",
+        state_class="measurement",
+    ),
+    
+    ModbusFieldConfig( 
+        name="digin_stoerungen",
+        address=33045,
+        unit="",
+        device_class=None,
+        state_class=None,
+        multiplier=1,
+        entity_category="diagnostic",
+    ),
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        ModbusFieldConfig(  # HKR1 Absenktemperatur Nacht
+        name="hkr1_absenktemperatur_nacht",
+        address=2821,
+        unit="°C",
+        device_class="temperature",
+        state_class="measurement",
+        register=2,
+        multiplier=1,
+        edit=True,
+        data=(5, 75),
+    ), 
+    
+    
+    
     ### General ###
     ModbusFieldConfig(  # Brennerleistung
         name="gas_power",
         address=33539,
         unit="kW",
         device_class="power",
-        state_class="measurement",
-    ),
-    ModbusFieldConfig(  # Außentemperatur
-        name="outdoor_air_temp",
-        address=33033,
-        unit="°C",
-        device_class="temperature",
         state_class="measurement",
     ),
     ModbusFieldConfig(  # Laufzeit Brenner
@@ -64,85 +265,10 @@ REGISTERS = [
         entity_category="diagnostic",
         absolut_value=True,
     ),
-    ModbusFieldConfig(  # DigIn Stoerungen
-        name="digin_stoerungen",
-        address=33045,
-        unit="",
-        device_class=None,
-        state_class=None,
-        multiplier=1,
-        entity_category="diagnostic",
-    ),
-    ModbusFieldConfig(  # VersionSC2
-        name="version_sc2",
-        address=32770,
-        unit="",
-        device_class=None,
-        state_class=None,
-        multiplier=1,
-        entity_category="diagnostic",
-    ),
-    ModbusFieldConfig(  # VersionNBG
-        name="version_nbg",
-        address=32771,
-        unit="",
-        device_class=None,
-        state_class=None,
-        multiplier=1,
-        entity_category="diagnostic",
-    ),
-    
-    ### Schichtspeicher ###
-    ModbusFieldConfig(  # Speicherreferenztemperatur
-        name="tank_layer1_water_temp",
-        address=33026,
-        unit="°C",
-        device_class="temperature",
-        state_class="measurement",
-    ),
-    ModbusFieldConfig(  # Heizungspuffertemperatur unten
-        name="tank_layer2_water_temp",
-        address=33032,
-        unit="°C",
-        device_class="temperature",
-        state_class="measurement",
-        negative=True,
-        absolut_value=True,
-    ),
-    ModbusFieldConfig(  # Heizungspuffertemperatur oben
-        name="tank_layer3_water_temp",
-        address=33027,
-        unit="°C",
-        device_class="temperature",
-        state_class="measurement",
-    ),
-    ModbusFieldConfig(  # Warmwasserpuffer
-        name="tank_layer4_water_temp",
-        address=33024,
-        unit="°C",
-        device_class="temperature",
-        state_class="measurement",
-    ),
-    
-    ## Heizkreis ###
-    ModbusFieldConfig(  # Vorlauftemperatur
-        name="flow_water_temp",
-        address=33035,
-        unit="°C",
-        device_class="temperature",
-        state_class="measurement",
-    ),
-    ModbusFieldConfig(  # HKR1 Absenktemperatur Nacht
-        name="hkr1_absenktemperatur_nacht",
-        address=2821,
-        unit="°C",
-        device_class="temperature",
-        state_class="measurement",
-        register=2,
-        multiplier=1,
-        edit=True,
-        data=(5, 75),
-    ),
+
+   
+
+
     ModbusFieldConfig(  # HKR1 Solltemperatur Tag
         name="hkr1_solltemperatur_tag",
         address=2820,
@@ -187,94 +313,8 @@ REGISTERS = [
         edit=True,
         data=(10, 65),
     ),
-    ModbusFieldConfig(  # Warmwassertemperatur
-        name="domestic_water_temp",
-        address=33025,
-        unit="°C",
-        device_class="temperature",
-        state_class="measurement",
-    ),
-    ModbusFieldConfig(  # Kaltwassertemperatur
-        name="cold_water_temperatur",
-        address=33038,
-        unit="°C",
-        device_class="temperature",
-        state_class="measurement",
-    ),
-    ModbusFieldConfig(  # Zirkulationsdurchfluss
-        name="cold_water_temp",
-        address=33034,
-        unit="°C",
-        device_class="temperature",
-        state_class="measurement",
-        multiplier=0.1,
-    ),    
-    ModbusFieldConfig(  # Durchfluss Warmwasserzirkualation
-        name="domestic_water_flow",
-        address=33041,
-        unit="l/min",
-        device_class="volume_flow_rate",
-        state_class="measurement",
-    ),
-    ModbusFieldConfig(  # ZirkulationBetriebsart
-        name="zirkulation_betriebsart",
-        address=2049,
-        unit="",
-        device_class=None,
-        state_class=None,
-        multiplier=1,
-        # data=("0", "1", "2", "3"),
-    ),
-
-    ### Solar ###
-    ModbusFieldConfig(
-        name="roof_air_temp",
-        address=33031,
-        unit="°C",
-        device_class="temperature",
-        state_class="measurement",
-        enabled_by_default=False,
-    ),
-    ModbusFieldConfig( #S16 (Nur bei OstWest Anlage)
-        name="solar_collector_temp_2",
-        address=33039,
-        unit="°C",
-        device_class="temperature",
-        state_class="measurement",
-        enabled_by_default=False,
-    ),
-    ModbusFieldConfig(
-        name="solar_water_temp",
-        address=33030,
-        unit="°C",
-        device_class="temperature",
-        state_class="measurement",
-        enabled_by_default=False,
-    ),
-    ModbusFieldConfig(
-        name="solar_heat_exchanger_in_water_temp",
-        address=33029,
-        unit="°C",
-        device_class="temperature",
-        state_class="measurement",
-        enabled_by_default=False,
-    ),
-    ModbusFieldConfig(
-        name="solar_heat_exchanger_out_water_temp",
-        address=33028,
-        unit="°C",
-        device_class="temperature",
-        state_class="measurement",
-        enabled_by_default=False,
-    ),    
-    ModbusFieldConfig(
-        name="solar_water_flow",
-        address=33040,
-        unit="l/min",
-        device_class=None,
-        state_class="measurement",
-        enabled_by_default=False,
-    ),
+    
+    
 
     ### Weitere ###
     ModbusFieldConfig(  # Ionisationsstrom
